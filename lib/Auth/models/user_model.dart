50a501c52d4d558aba2b6abcf334c7f141857639
charmeee <mandugohigh@gmail.com>
2023-03-25 00:51:38 +0900

    fix: 현 api에 맞게 변경

--- lib/Auth/models/user_model.dart
+++ lib/Auth/models/user_model.dart
@@ -1,35 +1,15 @@
 class UserInfo {
-  String? id;
-  String? username;
-  String? nickname;
-  String? userEmail;
-  String? ageRange;
-  String? gender;
-  String? weight;
+  String id;
+  String nickname;
+
   UserInfo(
-      {this.id,
-        this.username,
-        this.nickname,
-        this.userEmail,
-        this.ageRange,
-        this.gender,
-        this.weight});
+      {required this.id,
+        required this.nickname});
+
   UserInfo.fromJson(Map<String, dynamic> json)
       : id = json['id'],
-        username = json['username'],
-        nickname = json['nickname'],
-        userEmail = json['userEmail'],
-        ageRange = json['ageRange'],
-        weight = json['weight'],
-        gender = json['gender'];
+        nickname = json['nickname'];
 
-  UserInfo.init() {
-    id = null;
-    username = null;
-    nickname = null;
-    userEmail = null;
-    ageRange = null;
-    weight = null;
-    gender = null;
-  }
+
 }
+
