72f1861afb28a8f9d45c6330597880a7343c2344
charmeee <mandugohigh@gmail.com>
2023-03-25 00:50:26 +0900

    feat(ui): 로그인 ui 추가

--- lib/Auth/view/signIn.dart
+++ lib/Auth/view/signIn.dart
@@ -3,17 +3,64 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../widgets/signInForm.dart';
+
 class SignInPage extends ConsumerWidget {
-  const SignInPage({Key? key}) : super(key: key);
+  SignInPage({Key? key}) : super(key: key);
+
+
+
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sign In'),
-      ),
-      body: const Center(
-        child: Text('Sign In'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.2)),
+              Center(
+                child: SizedBox(
+                  height: 100,
+                  child:Column(
+                    children: [
+                      Text(
+                        '생자대 생활 정보톡',
+                        style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        '로그인',
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                    ],
+                  ),
+                )
+
+              ),
+
+              Padding(padding: EdgeInsets.only(top: 10)),
+
+              SignInForm(),
+              SizedBox(
+                height: 200,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/Auth/widgets/signInForm.dart
@@ -0,0 +1,111 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../auth_provider.dart';
+import '../view/signUp.dart';
+
+class SignInForm extends ConsumerStatefulWidget {
+  const SignInForm({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<SignInForm> createState() => _EmailSignInState();
+}
+
+class _EmailSignInState extends ConsumerState<SignInForm> {
+  final _signInFormKey = GlobalKey<FormState>();
+  String username = '';
+  String password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: ThemeData(
+          primaryColor: Colors.indigo,
+          inputDecorationTheme: InputDecorationTheme(
+              labelStyle: TextStyle(
+                color: Colors.indigo,
+                fontSize: 15,
+              ))),
+      child: Form(
+        key: _signInFormKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(labelText: '아이디를 입력해주세요'),
+              keyboardType: TextInputType.emailAddress,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '이메일을 입력해주세요';
+                }
+                return null;
+              },
+              onSaved: (value) => username = value!,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(labelText: '비밀번호를 입력해주세요'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '비밀번호를 입력해주세요';
+                }
+
+                return null;
+              },
+              onSaved: (value) => password = value!,
+              keyboardType: TextInputType.text,
+              inputFormatters: [//include ! ~ @ ?
+                FilteringTextInputFormatter.allow(RegExp(r'[a-zA-Z0-9!@~?]')),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            TextButton(
+                onPressed:() async {
+                  if(_signInFormKey.currentState!.validate()){
+                    _signInFormKey.currentState!.save();
+                    //authRepositoryProvider
+                    final resp = await ref.read(userNotifierProvider.notifier).signIn( username,password);
+                    log('resp'+resp.toString());
+                    //if resp has error then show error message
+                    if(resp.hasError){
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(resp.errorMessage!)));
+                    }
+
+                  }
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.indigo,
+                  padding: EdgeInsets.all(20),
+                ),
+                child: Text("로그인하기", style: TextStyle(color: Colors.white))),
+            SizedBox(
+                height: 10
+            ),
+            TextButton(
+                onPressed:(){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SignUpPage(),
+                    ),
+                  );
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.grey,
+                  padding: EdgeInsets.all(20),
+                ),
+                child: Text("회원가입", style: TextStyle(color: Colors.white)))
+          ],
+        ),
+      ),
+    );
+  }
+}
