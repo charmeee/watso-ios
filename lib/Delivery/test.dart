09dfe552cf706f925c38f0cbea8dd3d714aa21ac
charmeee <mandugohigh@gmail.com>
2023-03-31 22:06:42 +0900

    test: [get] post test json 생성

---
+++ lib/Delivery/test.dart
@@ -0,0 +1,44 @@
+const test = [
+  {
+    "_id": "63da17abcc800e43227d91e4",
+    "title": "[15:30] 네네치킨",
+    "user_id": 1674995732373,
+    "nickname": "개발이",
+    "place": "기숙사",
+    "min_member": 3,
+    "max_member": 6,
+    "order_time": "2023-02-22T08:56:57",
+    "store": {
+      "_id": "6345a45f1c32cd7c4b64d895",
+      "name": "맘스터치",
+      "min_order": 12000,
+      "fee": 3000
+    },
+    "open": true,
+    "order_completed": false,
+    "user_orders": [
+      {"user_id": 1674995732373, "nickname": "개발이"}
+    ]
+  },
+  {
+    "_id": "63da17abcc800e43227d91e4",
+    "title": "[15:30] 네네치킨",
+    "user_id": 1674995732373,
+    "nickname": "개발이",
+    "place": "기숙사",
+    "min_member": 3,
+    "max_member": 6,
+    "order_time": "2023-02-22T08:56:57",
+    "store": {
+      "_id": "6345a45f1c32cd7c4b64d895",
+      "name": "맘스터치",
+      "min_order": 12000,
+      "fee": 3000
+    },
+    "open": true,
+    "order_completed": false,
+    "user_orders": [
+      {"user_id": 1674995732373, "nickname": "개발이"}
+    ]
+  }
+];
