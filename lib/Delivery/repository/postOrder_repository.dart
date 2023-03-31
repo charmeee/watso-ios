4a5f5f3bf0e555e712b3d0119ef5abfcefb73232
charmeee <mandugohigh@gmail.com>
2023-03-31 22:09:05 +0900

    feat(api): getDeliveryList api 추가

---
+++ lib/Delivery/repository/postOrder_repository.dart
@@ -0,0 +1,36 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../Common/dio.dart';
+import '../models/post_response_model.dart';
+
+final postOrderRepositoryProvider = Provider<PostOrderRepository>(
+  (ref) {
+    final dio = ref.watch(dioProvider);
+
+    const staticUrl = '/api/delivery/post';
+
+    return PostOrderRepository(dio, staticUrl);
+  },
+);
+
+class PostOrderRepository {
+  PostOrderRepository(this._dio, this.staticUrl);
+
+  final String staticUrl;
+  final Dio _dio;
+
+  Future<List<ResponsePostList>> getDeliveryList() async {
+    try {
+      final response = await _dio.get(staticUrl);
+      return (response.data as List)
+          .map((e) => ResponsePostList.fromJson(e))
+          .toList();
+    } catch (e) {
+      log(e.toString());
+      throw Exception(e);
+    }
+  }
+}
