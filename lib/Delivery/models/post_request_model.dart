dfccac791a2b3027cb7cae46cd0f14237fe68815
charmeee <mandugohigh@gmail.com>
2023-04-01 06:31:32 +0900

    feat(model): 초기화와 오더가능한지 여부 속성 추가

--- lib/Delivery/models/post_request_model.dart
+++ lib/Delivery/models/post_request_model.dart
@@ -5,21 +5,36 @@ class PostOrder extends PostOption {
   List<Order> orders;
   String storeId;
 
-  PostOrder(
-      {required this.orders,
-      required this.storeId,
-      required String place,
-      required DateTime orderTime,
-      required int minMember,
-      required int maxMember})
+  PostOrder({required this.orders,
+    required this.storeId,
+    required String place,
+    required DateTime orderTime,
+    required int minMember,
+    required int maxMember})
       : super(
-            place: place,
-            orderTime: orderTime,
-            minMember: minMember,
-            maxMember: maxMember);
+      place: place,
+      orderTime: orderTime,
+      minMember: minMember,
+      maxMember: maxMember);
 
   PostOrder.fromJson(Map<String, dynamic> json)
       : orders = List<Order>.from(json['orders'].map((x) => Order.fromJson(x))),
         storeId = json['store_id'],
         super.fromJson(json);
+
+  factory PostOrder.init() {
+    return PostOrder(
+        orders: [],
+        storeId: '',
+        place: '',
+        orderTime: DateTime.now(),
+        minMember: 0,
+        maxMember: 999);
+  }
+
+
+  get canNotOrder =>
+      orders.isEmpty || storeId.isEmpty || place.isEmpty ||
+          orderTime.isBefore(DateTime.now()) || minMember > maxMember;
+
 }
