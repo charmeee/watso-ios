6c5a77f4d1bd99b10803393f3cbcb51b075c194d
charmeee <mandugohigh@gmail.com>
2023-03-31 22:01:58 +0900

    feat: userprofile state에 맞는 userinfo model추가

--- lib/Auth/models/user_model.dart
+++ lib/Auth/models/user_model.dart
@@ -1,4 +1,24 @@
-class UserInfo {
+class UserInfo extends User {
+  String username;
+  String accountNumber;
+  String email;
+
+  UserInfo(
+      {required id,
+      required nickname,
+      required this.username,
+      required this.accountNumber,
+      required this.email})
+      : super(id: id, nickname: nickname);
+
+  UserInfo.fromJson(Map<String, dynamic> json)
+      : username = json['username'],
+        accountNumber = json['account_number'],
+        email = json['email'],
+        super.fromJson(json);
+}
+
+class User {
   String id;
   String nickname;
 
