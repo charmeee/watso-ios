c1fc450a15067e2475c1f278781e5d821d7bd955
charmeee <mandugohigh@gmail.com>
2023-04-02 03:54:12 +0900

    feat(api): 상세 가게 조회 api 추가

--- lib/Delivery/repository/store_repository.dart
+++ lib/Delivery/repository/store_repository.dart
@@ -5,6 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../Common/dio.dart';
 import '../models/post_model.dart';
+import '../models/post_response_model.dart';
 
 final storeRepositoryProvider = Provider<StoreRepository>(
   (ref) {
@@ -26,6 +27,16 @@ class StoreRepository {
     try {
       final response = await _dio.get(staticUrl);
       return (response.data as List).map((e) => Store.fromJson(e)).toList();
+    } catch (e) {
+      log(e.toString());
+      throw Exception(e);
+    }
+  }
+
+  Future<StoreMenus> getStoreMenuList(storeId) async {
+    try {
+      final response = await _dio.get('$staticUrl/$storeId');
+      return StoreMenus.fromJson(response.data);
     } catch (e) {
       log(e.toString());
       throw Exception(e);
