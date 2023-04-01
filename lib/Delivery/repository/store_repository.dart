f17257b414143ce63f734f22c6c0d5217e0d1265
charmeee <mandugohigh@gmail.com>
2023-04-01 19:32:42 +0900

    feat(api): store get api 추가

---
+++ lib/Delivery/repository/store_repository.dart
@@ -0,0 +1,34 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../Common/dio.dart';
+import '../models/post_model.dart';
+
+final storeRepositoryProvider = Provider<StoreRepository>(
+  (ref) {
+    final dio = ref.watch(dioProvider);
+
+    const staticUrl = '/api/delivery/store';
+
+    return StoreRepository(dio, staticUrl);
+  },
+);
+
+class StoreRepository {
+  StoreRepository(this._dio, this.staticUrl);
+
+  final String staticUrl;
+  final Dio _dio;
+
+  Future<List<Store>> getStoreList() async {
+    try {
+      final response = await _dio.get(staticUrl);
+      return (response.data as List).map((e) => Store.fromJson(e)).toList();
+    } catch (e) {
+      log(e.toString());
+      throw Exception(e);
+    }
+  }
+}
