27380455e89ca601b5c3891d9514127e89e7e932
charmeee <mandugohigh@gmail.com>
2023-04-04 08:55:53 +0900

    feat: 가격 표시 기능 추가, 로직 개편

---
+++ lib/Delivery/models/menu_option_provider.dart
@@ -0,0 +1,99 @@
+import 'dart:developer';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../repository/store_repository.dart';
+import 'post_model.dart';
+
+final menuOptionNotifierProvider =
+    StateNotifierProvider<MenuOptionNotifier, OrderMenu?>((ref) {
+  return MenuOptionNotifier(ref);
+});
+
+class MenuOptionNotifier extends StateNotifier<OrderMenu?> {
+  MenuOptionNotifier(this.ref) : super(null);
+
+  final Ref ref;
+
+  Future<Menu> setMenu(storeId, menuName) async {
+    try {
+      final Menu value = await ref
+          .read(storeRepositoryProvider)
+          .getDetailMenu(storeId, menuName);
+      log('value: ${value.groups![0].options.length}');
+
+      state = OrderMenu.fromMenu(quantity: 1, menu: Menu.clone(value));
+      setLeastOneOption();
+      return value;
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  setLeastOneOption() {
+    final tmp = OrderMenu.clone(state!);
+    for (var element in tmp.menu.groups!) {
+      if (element.minOptionNum > 0) {
+        final option = element.options[0];
+        element.options = [option];
+      } else {
+        element.options = [];
+      }
+    }
+    log('state: ${state!.menu.groups![0].options[0].name}');
+    state = tmp;
+  }
+
+  void decreaseQuantity() {
+    final tmp = OrderMenu.clone(state!);
+    if (tmp.quantity == 1) return;
+    tmp.quantity--;
+    state = tmp;
+  }
+
+  void increaseQuantity() {
+    final tmp = OrderMenu.clone(state!);
+    tmp.quantity++;
+    state = tmp;
+  }
+
+  setOption(bool isRadio, String optionGroupId, MenuOption option) {
+    final tmp = OrderMenu.clone(state!);
+    if (isRadio) {
+      tmp.menu.groups!
+          .firstWhere((element) => element.id == optionGroupId)
+          .options = [option];
+    } else {
+      for (var i = 0; i < tmp.menu.groups!.length; i++) {
+        if (tmp.menu.groups![i].id == optionGroupId) {
+          for (var j = 0; j < tmp.menu.groups![i].options.length; j++) {
+            if (tmp.menu.groups![i].options[j].id == option.id) {
+              tmp.menu.groups![i].options.removeAt(j);
+              state = tmp;
+              return;
+            }
+          }
+          tmp.menu.groups![i].options.add(option);
+          state = tmp;
+          return;
+        }
+      }
+    }
+    state = tmp;
+  }
+}
+
+final sumPriceProvider = Provider<int>((ref) {
+  final orderMenu = ref.watch(menuOptionNotifierProvider);
+  if (orderMenu == null) {
+    return 0;
+  }
+  var sum = 0;
+  sum += orderMenu.menu.price * orderMenu.quantity;
+  for (var element in orderMenu.menu.groups!) {
+    for (var option in element.options) {
+      sum += option.price;
+    }
+  }
+  return sum;
+});
--- lib/Delivery/view/menu_option_page.dart
+++ lib/Delivery/view/menu_option_page.dart
@@ -1,11 +1,13 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sangsangtalk/Common/widget/appbar.dart';
 import 'package:sangsangtalk/Common/widget/floating_bottom_button.dart';
 
 import '../../Common/commonType.dart';
+import '../models/menu_option_provider.dart';
 import '../models/post_model.dart';
-import '../repository/store_repository.dart';
 
 class MenuOptionPage extends ConsumerStatefulWidget {
   const MenuOptionPage({
@@ -33,10 +35,10 @@ class _MenuOptionPageState extends ConsumerState<MenuOptionPage> {
 
   getDetailMenu() {
     ref
-        .read(storeRepositoryProvider)
-        .getDetailMenu(widget.storeId, widget.menuName)
+        .read(menuOptionNotifierProvider.notifier)
+        .setMenu(widget.storeId, widget.menuName)
         .then((value) {
-      setLeastOneOption(value);
+      log('value: ${value.groups![0].options.length}');
       setState(() {
         menu = value;
       });
@@ -47,25 +49,21 @@ class _MenuOptionPageState extends ConsumerState<MenuOptionPage> {
     });
   }
 
-  setLeastOneOption(Menu value) {
-    for (var element in value.groups!) {
-      if (element.minOptionNum > 0) {
-        selectedOptionsIdByGroupId[element.id] = [element.options[0].id];
-      } else {
-        selectedOptionsIdByGroupId[element.id] = [];
-      }
-    }
-  }
-
   //MenuOptionGroup 로 해서
   @override
   Widget build(BuildContext context) {
+    OrderMenu? orderMenu = ref.watch(menuOptionNotifierProvider);
+    int sumPrice = ref.watch(sumPriceProvider);
+    // //orderMenu logging
+    // if (orderMenu != null) {
+    //   log('orderMenu: ${jsonEncode(orderMenu.toJson())}');
+    // }
     if (loadState == LoadState.loading && menu == null) {
       return Scaffold(
         body: const Center(child: CircularProgressIndicator()),
       );
     }
-    if (menu == null || loadState == LoadState.error) {
+    if (menu == null || loadState == LoadState.error || orderMenu == null) {
       return Scaffold(
         body: const Center(child: Text('에러')),
       );
@@ -73,21 +71,53 @@ class _MenuOptionPageState extends ConsumerState<MenuOptionPage> {
     return Scaffold(
       appBar: customAppBar(
         context,
-        title: menu!.name,
+        title: menu!.name + ' : ' + menu!.price.toString() + '원',
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: ListView.separated(
-          itemCount: menu!.groups!.length,
+          itemCount: menu!.groups!.length + 1,
           shrinkWrap: true,
           padding: EdgeInsets.zero,
           physics: const BouncingScrollPhysics(),
           itemBuilder: (context, index) {
+            if (index == menu!.groups!.length) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('개수', style: const TextStyle(fontSize: 20)),
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            ref
+                                .read(menuOptionNotifierProvider.notifier)
+                                .decreaseQuantity();
+                          },
+                          icon: const Icon(Icons.remove),
+                        ),
+                        Text('${orderMenu.quantity}개'),
+                        IconButton(
+                          onPressed: () {
+                            ref
+                                .read(menuOptionNotifierProvider.notifier)
+                                .increaseQuantity();
+                          },
+                          icon: const Icon(Icons.add),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }
             final MenuOptionGroup optionGroup = menu!.groups![index];
+            List<MenuOption> selectedOptions =
+                orderMenu.menu.groups![index].options;
             final bool isRadio =
                 optionGroup.minOptionNum == 1 && optionGroup.maxOptionNum == 1;
-            //minOptionNum==maxOptionNum이면 radio
-            //minOptionNum<maxOptionNum이면 checkbox
             final selectIcon =
                 isRadio ? Icons.radio_button_checked : Icons.check_box_outlined;
             final unSelectIcon = isRadio
@@ -97,13 +127,19 @@ class _MenuOptionPageState extends ConsumerState<MenuOptionPage> {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               mainAxisSize: MainAxisSize.min,
               children: [
-                Text(optionGroup.name, style: const TextStyle(fontSize: 20)),
-                Row(
-                  children: [
-                    Text('최소: ${optionGroup.minOptionNum}'),
-                    const SizedBox(width: 10),
-                    Text('최대: ${optionGroup.maxOptionNum}'),
-                  ],
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(optionGroup.name,
+                          style: const TextStyle(fontSize: 20)),
+                      const SizedBox(width: 10),
+                      Text(isRadio
+                          ? '필수'
+                          : '최대 ${optionGroup.maxOptionNum} 선택 가능'),
+                    ],
+                  ),
                 ),
                 if (optionGroup.options.isNotEmpty)
                   for (var i = 0; i < optionGroup.options.length; i++)
@@ -115,38 +151,37 @@ class _MenuOptionPageState extends ConsumerState<MenuOptionPage> {
                             IconButton(
                                 onPressed: () {
                                   setState(() {
-                                    if (isRadio) {
-                                      selectedOptionsIdByGroupId[optionGroup
-                                          .id] = [optionGroup.options[i].id];
-                                    } else {
-                                      selectedOptionsIdByGroupId[
-                                              optionGroup.id]!
-                                          .add(optionGroup.options[i].id);
-                                    }
+                                    ref
+                                        .read(
+                                            menuOptionNotifierProvider.notifier)
+                                        .setOption(isRadio, optionGroup.id,
+                                            optionGroup.options[i]);
                                   });
                                 },
-                                icon: Icon(
-                                    selectedOptionsIdByGroupId[optionGroup.id]!
-                                            .contains(optionGroup.options[i].id)
-                                        ? selectIcon
-                                        : unSelectIcon)),
+                                icon: Icon(selectedOptions.any((element) =>
+                                        element.id == optionGroup.options[i].id)
+                                    ? selectIcon
+                                    : unSelectIcon)),
                             Text(optionGroup.options[i].name),
                           ],
                         ),
-                        Text('${optionGroup.options[i].price}개'),
+                        Text('${optionGroup.options[i].price}원'),
                       ],
                     ),
               ],
             );
           },
           separatorBuilder: (context, index) => const Divider(
-            height: 2,
+            height: 3,
+            thickness: 2,
           ),
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: customFloatingBottomButton(context,
-          child: Text('메뉴 담기'), onPressed: () {}),
+          child: Text('$sumPrice원 담기'), onPressed: () {}),
     );
   }
 }
+//todo: 메뉴 담기 기능
+//todo: 개수 조절 마지막에 추가
