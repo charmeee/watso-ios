7576e421707488e86a27d1593beb08d5e9394b65
charmeee <mandugohigh@gmail.com>
2023-04-27 20:53:31 +0900

    feat: auth api들 추가

---
+++ lib/Auth/repository/auth_repository.dart
@@ -0,0 +1,58 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+import '../../Common/dio.dart';
+import '../../Common/failures.dart';
+
+final authRepositoryProvider = Provider<AuthRepository>(
+  (ref) {
+    final dio = ref.watch(dioProvider);
+    final storage = ref.watch(secureStorageProvider);
+    const staticUrl = '/auth';
+
+    return AuthRepository(dio, storage, staticUrl);
+  },
+);
+
+class AuthRepository {
+  AuthRepository(this._dio, this.storage, this.staticUrl);
+
+  final String staticUrl;
+  final FlutterSecureStorage storage;
+  final Dio _dio;
+
+  Future<void> login(String username, String password) async {
+    try {
+      final response = await _dio.post('$staticUrl/signin', data: {
+        'username': username,
+        'pw': password,
+        'registration_token': 'string'
+      });
+      final token =
+          response.headers["authentication"]![0].toString().split("/");
+      await storage.delete(key: "accessToken");
+      await storage.write(key: "accessToken", value: token[0]);
+      await storage.delete(key: "refreshToken");
+      await storage.write(key: "refreshToken", value: token[1]);
+    } on DioError catch (e) {
+      throw ServerException(e);
+    } catch (e) {
+      throw TokenSetupException(e.toString());
+    }
+  }
+
+  Future<void> logout() async {
+    try {
+      await _dio.delete('$staticUrl/logout');
+      await storage.delete(key: "accessToken");
+      await storage.delete(key: "refreshToken");
+    } on DioError catch (e) {
+      await storage.delete(key: "accessToken");
+      await storage.delete(key: "refreshToken");
+      throw ServerException(e);
+    } catch (e) {
+      throw TokenSetupException(e.toString());
+    }
+  }
+}
