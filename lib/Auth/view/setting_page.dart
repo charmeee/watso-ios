df1e1625082001b77b5ad47f2bec362c37dc2140
charmeee <mandugohigh@gmail.com>
2023-04-30 03:30:23 +0900

    feat: settingpage 구현 완료

--- lib/Auth/view/setting_page.dart
+++ lib/Auth/view/setting_page.dart
@@ -1,24 +1,138 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sangsangtalk/Auth/models/user_model.dart';
 import 'package:sangsangtalk/Common/widget/appbar.dart';
 
-class SettingPage extends ConsumerStatefulWidget {
+import '../provider/user_provider.dart';
+
+class SettingPage extends ConsumerWidget {
   const SettingPage({
     Key? key,
   }) : super(key: key);
 
   @override
-  ConsumerState createState() => _SettingPageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    UserInfo userInfo = ref.watch(userNotifierProvider)!;
 
-class _SettingPageState extends ConsumerState<SettingPage> {
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: customAppBar(context, title: '설정'),
-      body: Center(
-        child: Text('설정'),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 8),
+                      child: Text(
+                        "계정",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text('이름'),
+                      subtitle: Text(userInfo.name),
+                      onTap: () {},
+                    ),
+                    Divider(height: 1),
+                    ListTile(
+                      title: Text('아이디'),
+                      subtitle: Text(userInfo.username),
+                      onTap: () {},
+                    ),
+                    Divider(height: 1),
+                    ListTile(
+                      title: Text('이메일'),
+                      subtitle: Text(userInfo.email),
+                      onTap: () {},
+                    ),
+                    Divider(height: 1),
+                    ListTile(
+                      title: Text('닉네임'),
+                      subtitle: Text(userInfo.nickname),
+                      trailing: Icon(Icons.arrow_forward_ios),
+                      onTap: () {},
+                    ),
+                    Divider(height: 1),
+                    ListTile(
+                      title: Text('계좌 번호'),
+                      subtitle: Text(userInfo.accountNumber),
+                      trailing: Icon(Icons.arrow_forward_ios),
+                      onTap: () {},
+                    ),
+                    Divider(height: 1),
+                    ListTile(
+                      title: Text('비밀번호'),
+                      subtitle: Text('********'),
+                      trailing: Icon(Icons.arrow_forward_ios),
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 8),
+                      child: Text(
+                        "알림",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text('알림 설정'),
+                      trailing: Switch(
+                        value: true,
+                        onChanged: (value) {},
+                        activeTrackColor: Colors.indigo[100],
+                        activeColor: Colors.indigo,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
       ),
     );
   }
 }
+//
+// class SettingPage extends ConsumerStatefulWidget {
+//   const SettingPage({
+//     Key? key,
+//   }) : super(key: key);
+//
+//   @override
+//   ConsumerState createState() => _SettingPageState();
+// }
+//
+// class _SettingPageState extends ConsumerState<SettingPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: customAppBar(context, title: '설정'),
+//       body: Center(
+//         child: Text('설정'),
+//       ),
+//     );
+//   }
+// }
