b152f8cae83af8fbfed453602c9c478f0dbb65c3
charmeee <mandugohigh@gmail.com>
2023-04-29 23:42:07 +0900

    feat: 새로고침할 수 있도록 리스트 불러오는 api를 futureprovider로 추가

--- lib/Delivery/provider/post_list_provider.dart
+++ lib/Delivery/provider/post_list_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sangsangtalk/Delivery/models/comment_model.dart';
 import 'package:sangsangtalk/Delivery/repository/post_repository.dart';
 
 import '../models/post_model.dart';
@@ -13,4 +14,9 @@ final myPostListProvider = FutureProvider<List<ResponsePost>>((ref) async {
 final postDetailProvider = FutureProvider.autoDispose
     .family<ResponsePost, String>((ref, postId) async {
   return await ref.read(postRepositoryProvider).getPostDetail(postId);
+});
+
+final postCommentListProvider = FutureProvider.autoDispose
+    .family<List<ParentComment>, String>((ref, postId) async {
+  return await ref.read(postRepositoryProvider).getCommentList(postId);
 });
