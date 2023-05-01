e7df98e0d16beafc46079db2232f37a6c4a23eb6
charmeee <mandugohigh@gmail.com>
2023-05-01 22:45:44 +0900

    refactor: state안쓰고 프로바이더만

--- lib/Delivery/widgets/index_post_list.dart
+++ lib/Delivery/widgets/index_post_list.dart
@@ -1,84 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../Common/commonType.dart';
-import '../models/post_model.dart';
 import '../models/post_response_model.dart';
-import '../repository/post_repository.dart';
+import '../provider/post_list_provider.dart';
 import 'index_common_listTile.dart';
 
-class PostList extends ConsumerStatefulWidget {
+class PostList extends ConsumerWidget {
   const PostList({
     Key? key,
   }) : super(key: key);
 
   @override
-  ConsumerState createState() => _PostListState();
-}
-
-class _PostListState extends ConsumerState<PostList> {
-  List<ResponsePost> postData = [];
-  LoadState loadState = LoadState.loading;
-
-  @override
-  void initState() {
-    super.initState();
-    getPostData();
-  }
-
-  getPostData() {
-    ref
-        .read(postRepositoryProvider)
-        .getDeliveryList(PostFilter.joinable)
-        .then((value) {
-      setState(() {
-        postData = value;
-        loadState = LoadState.success;
-      });
-    }).onError((error, stackTrace) {
-      setState(() {
-        loadState = LoadState.error;
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (loadState == LoadState.loading) {
-      return Center(child: CircularProgressIndicator());
-    }
-    if (loadState == LoadState.error) {
-      return SizedBox(
-          child: Center(
-              child: Text(
-            '에러',
-            style: TextStyle(fontSize: 20),
-          )),
-          height: 300);
-    }
-    if (postData.isEmpty) {
-      return SizedBox(
-          child: Center(
-              child: Text(
-            '직접 배달톡을 추가해 보세요',
-            style: TextStyle(fontSize: 20),
-          )),
-          height: 300);
-    }
-
-    //to ListView
-    return ListView.separated(
-        itemBuilder: (BuildContext context, int index) {
-          final data = postData[index];
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 4),
-            child: indexCommonListTile(data, context),
-          );
-        },
-        separatorBuilder: (BuildContext context, int index) => Divider(),
-        shrinkWrap: true,
-        physics: NeverScrollableScrollPhysics(),
-        padding: EdgeInsets.zero,
-        itemCount: postData.length);
+  Widget build(BuildContext context, WidgetRef ref) {
+    AsyncValue<List<ResponsePost>> joinablePostList = ref.watch(
+        joinablePostListProvider);
+    return SizedBox(
+      child: joinablePostList.when(data: (data) {
+        return ListView.separated(
+            itemBuilder: (BuildContext context, int index) {
+              final nowData = data[index];
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 4),
+                child: indexCommonListTile(nowData, context),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) => Divider(),
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            padding: EdgeInsets.zero,
+            itemCount: data.length);
+      }, error: (error, stackTrace) {
+        return SizedBox(
+            child: Center(
+                child: Text(
+                  '에러',
+                  style: TextStyle(fontSize: 20),
+                )),
+            height: 300);
+      }, loading: () {
+        return Center(child: CircularProgressIndicator());
+      }),
+    );
   }
 }
