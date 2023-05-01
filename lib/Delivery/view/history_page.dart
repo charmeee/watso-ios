fe75796401fd6673ca2071e00641ed3e1926c6c3
charmeee <mandugohigh@gmail.com>
2023-05-01 22:38:55 +0900

    feat: 히스토리 페이지 추가

--- lib/Delivery/view/history_page.dart
+++ lib/Delivery/view/history_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 
 import '../../Common/widget/appbar.dart';
+import '../models/post_model.dart';
+import '../models/post_response_model.dart';
+import '../repository/post_repository.dart';
 
 class DeliverHistoryPage extends ConsumerWidget {
   const DeliverHistoryPage({
@@ -12,8 +16,102 @@ class DeliverHistoryPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: customAppBar(context, title: '배달톡 참가 내역'),
-      body: Center(
-        child: Text('배달톡 참가 내역'),
+      body: FutureBuilder<List<ResponsePost>>(
+        future:
+            ref.read(postRepositoryProvider).getDeliveryList(PostFilter.all),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            final List<ResponsePost> data = snapshot.data!;
+
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                ResponsePost nowData = data[index];
+                String orderDate =
+                    DateFormat("M.d(E)", 'ko').format(nowData.orderTime);
+                return Card(
+                  margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 8.0, horizontal: 16),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Text(orderDate),
+                            Text('﹒'),
+                            Text(nowData.status.korName),
+                            Spacer(),
+                            OutlinedButton(
+                              onPressed: () {},
+                              child: Text(
+                                '내 배달 보기',
+                                style:
+                                    TextStyle(height: 1, color: Colors.black),
+                              ),
+                              style: OutlinedButton.styleFrom(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 8, vertical: 4),
+                                textStyle: TextStyle(height: 1),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(4),
+                                ),
+                                visualDensity: VisualDensity.compact,
+                              ),
+                            )
+                          ],
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(
+                                nowData.store.name,
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                              Icon(
+                                Icons.arrow_forward_ios_rounded,
+                                size: 18,
+                              ),
+                            ],
+                          ),
+                          style: TextButton.styleFrom(
+                            foregroundColor: Colors.black,
+                            padding: EdgeInsets.zero,
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text('게시글 보러가기'),
+                          style: ElevatedButton.styleFrom(
+                              minimumSize: const Size.fromHeight(40),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                              backgroundColor: Colors.indigo[200]),
+                        )
+                        // icon: Icon(Icons.arrow_forward_ios),
+                        // label: Text('주문 내역 보기'))
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('에러'),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
