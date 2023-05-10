383de165caeb459bf24f473fa5ecc36e1d469694
charmeee <mandugohigh@gmail.com>
2023-05-11 00:09:49 +0900

    feat: 앱의 주요 color, text 구성

---
+++ lib/Common/theme/color.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class WatsoColor {
+  static const primary = Color(0xffff7700);
+  static const text = Color(0xff2c2c2c);
+  static const lightText = Color(0x8A000000);
+  static const agree = Color(0xff4caf50);
+}
---
+++ lib/Common/theme/text.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:watso/Common/theme/color.dart';
+
+class WatsoText {
+  //header
+  static const logo = TextStyle(
+    fontSize: 30,
+    fontFamily: 'ddangs',
+    color: WatsoColor.text,
+  );
+
+  static const normal = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w300,
+    fontFamily: 'suite',
+    color: WatsoColor.text,
+  );
+
+  static const bold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'suite',
+    color: WatsoColor.text,
+  );
+
+  static const readable = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'suite',
+    color: WatsoColor.text,
+  );
+
+  static const appBar = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w800,
+    fontFamily: 'suite',
+    color: WatsoColor.text,
+  );
+}
