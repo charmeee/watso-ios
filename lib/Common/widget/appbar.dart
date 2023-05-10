006d8dba9739933f95dc646aa26d6f974308757e
charmeee <mandugohigh@gmail.com>
2023-05-11 00:16:27 +0900

    feat: appbar에 로고인 경우를 추가해줌

--- lib/Common/widget/appbar.dart
+++ lib/Common/widget/appbar.dart
@@ -2,24 +2,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import '../theme/text.dart';
+
 PreferredSizeWidget customAppBar(context,
-    {String? title, int? titleSize, List<Widget>? action, bool? isCenter}) {
+    {String? title,
+    double? titleSize,
+    List<Widget>? action,
+    bool isCenter = false,
+    bool isLogo = false}) {
   return AppBar(
     iconTheme: const IconThemeData(
       color: Colors.black, //change your color here
     ),
     backgroundColor: Colors.transparent,
     elevation: 0,
-    centerTitle: isCenter ?? false,
+    centerTitle: isCenter,
     title: title != null
-        ? Text(
-            title,
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: titleSize?.toDouble(),
-              fontWeight: FontWeight.bold,
-            ),
-          )
+        ? (isLogo
+            ? Text(title, style: WatsoText.logo)
+            : Text(title,
+                style: WatsoText.appBar.copyWith(
+                  fontSize: titleSize ?? 22,
+                )))
         : null,
     actions: action,
     systemOverlayStyle: SystemUiOverlayStyle.dark,
