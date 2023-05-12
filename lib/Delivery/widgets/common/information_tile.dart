8444927d3006a295f9520130074460a5bd7385a7
charmeee <mandugohigh@gmail.com>
2023-05-12 14:14:49 +0900

    refactor: infomation tile widget분리

---
+++ lib/Delivery/widgets/common/information_tile.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class InformationTile extends StatelessWidget {
+  const InformationTile(
+      {Key? key,
+      required this.icon,
+      required this.title,
+      this.content,
+      this.widget})
+      : super(key: key);
+  final IconData icon;
+  final String title;
+  final String? content;
+  final Widget? widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: Icon(
+              icon,
+              // Icons.store,
+              color: Colors.grey,
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(title,
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey,
+                  )),
+              if (widget != null) widget!,
+              if (content != null)
+                Text(
+                  content!,
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
