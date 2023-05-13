afb140ad01390f4f093fb122191871d762a0fb45
charmeee <mandugohigh@gmail.com>
2023-05-13 19:59:09 +0900

    refactor: switch를 이용하여 컴포넌트 불러오는것으로 수정

--- lib/Auth/view/edit_page.dart
+++ lib/Auth/view/edit_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:watso/Common/widget/appbar.dart';
 
-import '../widgets/accountNumberEditBox.dart';
+import '../widgets/account_edit_box.dart';
 import '../widgets/nickname_edit_box.dart';
+import '../widgets/password_edit_box.dart';
 
 class UserEditPage extends StatelessWidget {
   const UserEditPage({
@@ -18,15 +19,29 @@ class UserEditPage extends StatelessWidget {
     return Scaffold(
       appBar: customAppBar(context, title: title),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 32),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            if (field == 'nickname') NickNameEditBox(),
-            if (field == 'accountNumber') AccountEditBox(),
-          ],
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Card(
+          elevation: 0,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 24),
+            child: _buildEditBox(field),
+          ),
         ),
+
       ),
     );
+  }
+
+  Widget _buildEditBox(String field) {
+    switch (field) {
+      case 'nickname':
+        return NickNameEditBox();
+      case 'accountNumber':
+        return AccountEditBox();
+      case 'password':
+        return PasswordEditBox();
+      default:
+        return SizedBox(height: 10,);
+    }
   }
 }
