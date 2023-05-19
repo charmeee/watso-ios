a2ff7efcd6480ec7cbe13ab05c9cb0b09de8c73d
charmeee <mandugohigh@gmail.com>
2023-05-20 04:58:22 +0900

    fix: 백 전달 객체 변경에 따른 필드 추가

--- lib/Delivery/models/post_response_model.dart
+++ lib/Delivery/models/post_response_model.dart
@@ -5,31 +5,35 @@ class ResponsePost extends PostOption {
   String id;
   String userId;
   String nickname;
+  int fee;
   Store store;
   PostStatus status;
   List<String> users;
 
-  ResponsePost({required this.id,
-    required this.userId,
-    required this.nickname,
-    required place,
-    required minMember,
-    required maxMember,
-    required orderTime,
-    required this.store,
-    required this.status,
-    required this.users})
+  ResponsePost(
+      {required this.id,
+      required this.userId,
+      required this.nickname,
+      required this.fee,
+      required place,
+      required minMember,
+      required maxMember,
+      required orderTime,
+      required this.store,
+      required this.status,
+      required this.users})
       : super(
-    place: place,
-    minMember: minMember,
-    maxMember: maxMember,
-    orderTime: orderTime,
-  );
+          place: place,
+          minMember: minMember,
+          maxMember: maxMember,
+          orderTime: orderTime,
+        );
 
   ResponsePost.fromJson(Map<String, dynamic> json)
       : id = json['_id'].toString(),
         userId = json['user_id'].toString(),
         nickname = json['nickname'],
+        fee = json['fee'],
         store = Store.fromJson(json['store']),
         status = PostStatus.values.byName(json['status']),
         users = List<String>.from(json['users'].map((x) => x.toString())),
@@ -39,25 +43,26 @@ class ResponsePost extends PostOption {
 class StoreMenus extends Store {
   List<MenuSection> menuSection;
 
-  StoreMenus({required String id,
-    required String name,
-    required int minOrder,
-    required int fee,
-    required String phoneNumber,
-    required String logoImgUrl,
-    required List<String> note,
-    required this.menuSection})
+  StoreMenus(
+      {required String id,
+      required String name,
+      required int minOrder,
+      required int fee,
+      required String phoneNumber,
+      required String logoImgUrl,
+      required List<String> note,
+      required this.menuSection})
       : super(
-      id: id,
-      name: name,
-      minOrder: minOrder,
-      fee: fee,
-      phoneNumber: phoneNumber,
-      logoImgUrl: logoImgUrl,
-      note: note);
+            id: id,
+            name: name,
+            minOrder: minOrder,
+            fee: fee,
+            phoneNumber: phoneNumber,
+            logoImgUrl: logoImgUrl,
+            note: note);
 
   StoreMenus.fromJson(Map<String, dynamic> json)
       : menuSection = List<MenuSection>.from(
-      json['sections'].map((x) => MenuSection.fromJson(x))),
+            json['sections'].map((x) => MenuSection.fromJson(x))),
         super.fromJson(json);
 }
