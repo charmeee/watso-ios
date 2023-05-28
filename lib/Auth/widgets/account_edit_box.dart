5337643d284f5d759fc689f286177176657653d6
charmeee <mandugohigh@gmail.com>
2023-05-29 07:04:59 +0900

    fix: 계좌수정 박스 디자인적용

--- lib/Auth/widgets/account_edit_box.dart
+++ lib/Auth/widgets/account_edit_box.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:watso/Common/widget/secondary_button.dart';
 
 import '../../Common/theme/text.dart';
 import '../../Common/widget/outline_textformfield.dart';
+import '../../Common/widget/primary_button.dart';
 import '../provider/user_provider.dart';
 import '../repository/user_repository.dart';
 
 class AccountEditBox extends ConsumerStatefulWidget {
   const AccountEditBox({
     Key? key,
+    this.isSecondary = false,
   }) : super(key: key);
+  final bool isSecondary;
 
   @override
   ConsumerState createState() => _AccountEditBoxState();
@@ -18,57 +22,60 @@ class AccountEditBox extends ConsumerStatefulWidget {
 class _AccountEditBoxState extends ConsumerState<AccountEditBox> {
   String accountNumber = '';
 
+  onPressSubmit() {
+    ref
+        .read(userRepositoryProvider)
+        .updateAccountNumber(accountNumber)
+        .then((value) {
+      ref
+          .read(userNotifierProvider.notifier)
+          .setUserInfo('accountNumber', accountNumber);
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('계좌번호가 변경되었습니다.')));
+      if (widget.isSecondary == false) {
+        Navigator.pop(context);
+      }
+    }).onError((error, stackTrace) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('계좌번호 변경에 실패했습니다.')));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Text(
           '새 계좌번호를 입력해주세요',
           style: WatsoText.lightBold,
         ),
         const SizedBox(height: 20),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: outlineTextFromField(
-            onChanged: (value) {
-              setState(() {
-                accountNumber = value;
-              });
-            },
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return '계좌번호를 입력해주세요';
-              }
-              return null;
-            },
-            hintText: '계좌번호를 입력해주세요',
-          ),
-        ),
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            foregroundColor: Colors.black,
-            backgroundColor: Colors.grey[300],
-          ),
-          onPressed: () {
-            ref
-                .read(userRepositoryProvider)
-                .updateAccountNumber(accountNumber)
-                .then((value) {
-              ref
-                  .read(userNotifierProvider.notifier)
-                  .setUserInfo('accountNumber', accountNumber);
-              ScaffoldMessenger.of(context)
-                  .showSnackBar(SnackBar(content: Text('계좌번호가 변경되었습니다.')));
-              Navigator.pop(context);
-            }).onError((error, stackTrace) {
-              ScaffoldMessenger.of(context)
-                  .showSnackBar(SnackBar(content: Text('계좌번호 변경에 실패했습니다.')));
+        outlineTextFromField(
+          onChanged: (value) {
+            setState(() {
+              accountNumber = value;
             });
           },
-          child: Text('계좌번호 변경'),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return '계좌번호를 입력해주세요';
+            }
+            return null;
+          },
+          hintText: '계좌번호를 입력해주세요',
         ),
+        SizedBox(height: 20),
+        widget.isSecondary
+            ? secondaryButton(
+                onPressed: onPressSubmit,
+                text: '계좌번호 변경',
+              )
+            : primaryButton(
+                onPressed: onPressSubmit,
+                child: Text('계좌번호 변경'),
+              ),
       ],
     );
   }
