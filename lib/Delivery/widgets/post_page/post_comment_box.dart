b1a052e7c0f06339cc10723dbf41f740350e26e6
charmeee <mandugohigh@gmail.com>
2023-05-30 19:14:02 +0900

    fix: 자식 댓글 디자인 변경

--- lib/Delivery/widgets/post_page/post_comment_box.dart
+++ lib/Delivery/widgets/post_page/post_comment_box.dart
@@ -28,122 +28,137 @@ class CommentBox extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    String createdAt = DateFormat("M.d h:m", 'ko').format(comment.createdAt);
+    String createdAt = DateFormat("M/d h:m", 'ko').format(comment.createdAt);
     final user = ref.watch(userNotifierProvider);
-    return Padding(
-      padding: isParent
-          ? const EdgeInsets.only()
-          : const EdgeInsets.only(left: 24.0),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: Container(
-          decoration: BoxDecoration(
-            color: selectedCommentId == comment.id
-                ? Colors.indigo[50]
-                : Colors.grey[50],
-            borderRadius: BorderRadius.circular(5),
+    return Row(
+      children: [
+        if (!isParent)
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              '↳',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
           ),
-          padding: EdgeInsets.all(10),
-          child: comment.status == CommentStatus.deleted
-              ? Text("삭제된 댓글입니다.")
-              : Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Row(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: selectedCommentId == comment.id
+                    ? Colors.indigo[50]
+                    : Colors.grey[50],
+                borderRadius: BorderRadius.circular(5),
+              ),
+              padding: EdgeInsets.all(10),
+              child: comment.status == CommentStatus.deleted
+                  ? Text("삭제된 댓글입니다.")
+                  : Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
                       children: [
-                        Text("${comment.nickname} · $createdAt ",
-                            style: TextStyle(
-                              fontSize: 16,
-                            )),
-                        if (user?.id == comment.userId)
-                          InkWell(
-                            child: Icon(
-                              Icons.close,
-                              size: 14,
-                            ),
-                            onTap: () {
-                              showDialog(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return AlertDialog(
-                                      title: Text("댓글 삭제"),
-                                      content: Text("댓글을 삭제하시겠습니까?"),
-                                      actions: [
-                                        TextButton(
-                                            onPressed: () {
-                                              Navigator.of(context).pop();
-                                            },
-                                            child: Text("취소")),
-                                        TextButton(
-                                            onPressed: () {
-                                              ref
-                                                  .read(postRepositoryProvider)
-                                                  .deleteComment(comment.postId,
-                                                      comment.id)
-                                                  .then((value) => ref.invalidate(
-                                                      postCommentListProvider(
-                                                          comment.postId)))
-                                                  .onError((error, stackTrace) {
-                                                ScaffoldMessenger.of(context)
-                                                    .showSnackBar(SnackBar(
-                                                        content: Text(
-                                                            "댓글 삭제에 실패했습니다.")));
-                                              });
-                                              Navigator.of(context).pop();
-                                            },
-                                            child: Text("삭제")),
-                                      ],
-                                    );
-                                  });
-                            },
-                          )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 4,
-                    ),
-                    Flexible(
-                        child: Text(
-                      comment.content,
-                      style: TextStyle(color: Colors.black),
-                    )),
-                    SizedBox(
-                      height: 4,
-                    ),
-                    if (isParent)
-                      InkWell(
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Icon(
-                              Icons.comment,
-                              size: 16,
-                            ),
-                            Text(
-                              "답글달기",
-                              style: TextStyle(fontSize: 16),
-                            ),
+                            Text("${comment.nickname} · $createdAt ",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                )),
+                            if (user?.id == comment.userId)
+                              InkWell(
+                                child: Icon(
+                                  Icons.close,
+                                  size: 14,
+                                ),
+                                onTap: () {
+                                  showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return AlertDialog(
+                                          title: Text("댓글 삭제"),
+                                          content: Text("댓글을 삭제하시겠습니까?"),
+                                          actions: [
+                                            TextButton(
+                                                onPressed: () {
+                                                  Navigator.of(context).pop();
+                                                },
+                                                child: Text("취소")),
+                                            TextButton(
+                                                onPressed: () {
+                                                  ref
+                                                      .read(
+                                                          postRepositoryProvider)
+                                                      .deleteComment(
+                                                          comment.postId,
+                                                          comment.id)
+                                                      .then((value) =>
+                                                          ref.invalidate(
+                                                              postCommentListProvider(
+                                                                  comment
+                                                                      .postId)))
+                                                      .onError(
+                                                          (error, stackTrace) {
+                                                    ScaffoldMessenger.of(
+                                                            context)
+                                                        .showSnackBar(SnackBar(
+                                                            content: Text(
+                                                                "댓글 삭제에 실패했습니다.")));
+                                                  });
+                                                  Navigator.of(context).pop();
+                                                },
+                                                child: Text("삭제")),
+                                          ],
+                                        );
+                                      });
+                                },
+                              )
                           ],
                         ),
-                        onTap: () {
-                          if (selectedCommentId == comment.id) {
-                            selectComment('');
-                            commentFocusNode.unfocus();
-                          } else {
-                            selectComment(comment.id);
-                            commentFocusNode.requestFocus();
-                          }
-                        },
-                        highlightColor: Colors.red,
-                      ),
-                  ],
-                ),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Flexible(
+                            child: Text(
+                          comment.content,
+                          style: TextStyle(color: Colors.black),
+                        )),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        if (isParent)
+                          InkWell(
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Icon(
+                                  Icons.comment,
+                                  size: 16,
+                                ),
+                                Text(
+                                  "답글달기",
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                              ],
+                            ),
+                            onTap: () {
+                              if (selectedCommentId == comment.id) {
+                                selectComment('');
+                                commentFocusNode.unfocus();
+                              } else {
+                                selectComment(comment.id);
+                                commentFocusNode.requestFocus();
+                              }
+                            },
+                            highlightColor: Colors.red,
+                          ),
+                      ],
+                    ),
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
